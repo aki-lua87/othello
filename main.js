e2e5387d4231861cf05782e354f023d2566a5e33
aki_lua87 <aki_lua87@outlook.jp>
2023-09-05 21:48:16 +0900

    ネットワーク対戦

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 "use strict";
 
-const BLACK = 1,
-    WHITE = -1;
+const BLACK = 1, WHITE = -1;
 let data = [];
 let turn = true;
 const board = document.getElementById("board");
 const h2 = document.querySelector("h2");
+const infomation = document.getElementById("infomation");
 const counter = document.getElementById("counter");
-const modal = document.getElementById("modal");
-document.querySelectorAll(".select").forEach((value) => {
-    value.addEventListener("click", start);
-});
 let cells = 8; // マスの数
+let latestPutDisc = "";
 
-// スタート画面でマスの数が選択された時の処理
-function start(e) {
-    cells = Number(e.target.id);
+let MODE = 0; // 0:マッチAPI前 1:マッチ捜索中 2:自手待ち 3:受信(相手)待ち 4: 自身も手送信待ち 99:マッチ失敗
+const myId = crypto.randomUUID();
+let myCloer = BLACK;
+
+function start() {
     board.innerHTML = "";
     init();
     modal.classList.add("hide");
 }
 
-// 初期化
 function init() {
+    MODE = 0;
     for (let i = 0; i < cells; i++) {
         const tr = document.createElement("tr");
         data[i] = Array(cells).fill(0);
@@ -36,34 +35,114 @@ function init() {
         }
         board.appendChild(tr);
     }
-    // マスの数によって石の初期配置を変える
-    switch (cells) {
-        case 4:
-            board.classList.add("cell-4");
-            putDisc(1, 1, WHITE);
-            putDisc(2, 2, WHITE);
-            putDisc(1, 2, BLACK);
-            putDisc(2, 1, BLACK);
-            break;
-        case 6:
-            board.classList.add("cell-6");
-            putDisc(2, 2, WHITE);
-            putDisc(3, 3, WHITE);
-            putDisc(2, 3, BLACK);
-            putDisc(3, 2, BLACK);
-            break;
-        case 8:
-            putDisc(3, 3, WHITE);
-            putDisc(4, 4, WHITE);
-            putDisc(3, 4, BLACK);
-            putDisc(4, 3, BLACK);
-    }
+    putDisc(3, 3, WHITE);
+    putDisc(4, 4, WHITE);
+    putDisc(3, 4, BLACK);
+    putDisc(4, 3, BLACK);
+    // マッチングAPIコール
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://cwylm72ahf.execute-api.ap-northeast-1.amazonaws.com/dev/rv/entry/regist?terminal_id=' + myId, true);
+    request.responseType = 'json';
+    request.onload = function () {
+        var data = this.response;
+        console.log("rv/entry/regist");
+        console.log(data);
+        MODE = 1;
+        document.getElementById("infomation").textContent = "マッチ検索中です";
+        searchMatching();
+    };
+    request.send();
+
     showTurn();
 }
 
-init();
+function showMassege(massege) {
+    alert(massege);
+}
 
-// 石を描画
+function searchMatching() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://cwylm72ahf.execute-api.ap-northeast-1.amazonaws.com/dev/rv/entry/check?terminal_id=' + myId, true);
+    request.responseType = 'json';
+    request.onload = function () {
+        console.log("rv/entry/search");
+        var data = this.response;
+        console.log(data);
+        if (data.status == "MATCHED") {
+            MODE = 2;
+            let terminalID_A = data.terminal_id_A;
+            let terminalID_B = data.terminal_id_B;
+            if (myId == terminalID_A) {
+                // turn = true;
+                myCloer = BLACK;
+                document.getElementById("infomation").textContent = "あなたは黒番です";
+            } else {
+                MODE = 3;
+                // turn = false;
+                myCloer = WHITE;
+                document.getElementById("infomation").textContent = "あなたは白番です";
+                setTimeout(searchPutDisc, 5000); // 再帰なので注意
+            }
+        } else {
+            setTimeout(searchMatching, 5000); // 再帰なので注意
+        }
+    };
+    request.send();
+}
+
+
+function sendPutDisc(x, y) {
+    var request = new XMLHttpRequest();
+    let left = convertNumToAlfa(String(x));
+    latestPutDisc = left + (y + 1);
+    console.log("latestPutDisc:" + latestPutDisc);
+    request.open('GET', 'https://cwylm72ahf.execute-api.ap-northeast-1.amazonaws.com/dev/rv/action/regist?terminal_id=' + myId + '&action=' + latestPutDisc, true);
+    request.responseType = 'json';
+    request.onload = function () {
+        console.log("rv/action/regist");
+        var data = this.response;
+        console.log(data);
+        MODE = 3;
+        searchPutDisc();
+        document.getElementById("infomation").textContent = "送信しました:" + latestPutDisc;
+    };
+    request.send();
+}
+
+function searchPutDisc() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://cwylm72ahf.execute-api.ap-northeast-1.amazonaws.com/dev/rv/action/check?terminal_id=' + myId, true);
+    request.responseType = 'json';
+    request.onload = function () {
+        console.log("rv/action/check");
+        var data = this.response;
+        console.log(data);
+        let latest = data.latest;
+        if (latest === latestPutDisc) {
+            setTimeout(searchPutDisc, 5000); // 再帰なので注意
+            return;
+        }
+        let x = Number(convertAlfaToNum(latest[0]));
+        let y = latest[1];
+        clickedAitePlayer(x, y - 1);
+        MODE = 2;
+        document.getElementById("infomation").textContent = "受信しました:" + latest;
+    };
+    request.send();
+}
+
+function convertNumToAlfa(str) {
+    return str.replace("0", "A").replace("1", "B").replace("2", "C").replace("3", "D").replace("4", "E").replace("5", "F").replace("6", "G").replace("7", "H");
+}
+
+function convertAlfaToNum(str) {
+    return str.replace("A", "0").replace("B", "1").replace("C", "2").replace("D", "3").replace("E", "4").replace("F", "5").replace("G", "6").replace("H", "7");
+}
+
+
+// init();
+start();
+
 function putDisc(x, y, color) {
     board.rows[y].cells[x].firstChild.className =
         color === BLACK ? "black" : "white";
@@ -74,7 +153,6 @@ function putDisc(x, y, color) {
     data[y][x] = color;
 }
 
-// 手番などの表示
 function showTurn() {
     h2.textContent = turn ? "黒の番です" : "白の番です";
     let numWhite = 0,
@@ -133,16 +211,55 @@ function showTurn() {
     }
 }
 
-// マスがクリックされた時の処理
 function clicked() {
+    if (MODE !== 2) {
+        // 自手待ち状態以外はクリック無効
+        return;
+    }
     const color = turn ? BLACK : WHITE;
+    if (color !== myCloer) {
+        return;
+    }
     const y = this.parentNode.rowIndex;
     const x = this.cellIndex;
+    console.log("clicked :" + x + "," + y);
+
+    console.log(typeof x);
+    console.log(typeof y);
     // マスに置けるかチェック
     if (data[y][x] !== 0) {
         return;
     }
     const result = checkPut(x, y, color);
+    console.log("checkPut1:" + result.length);
+    if (result.length > 0) {
+        result.forEach((value) => {
+            putDisc(value[0], value[1], color);
+        });
+        turn = !turn;
+        sendPutDisc(x, y);
+    }
+    showTurn();
+}
+
+function clickedAitePlayer(x, y) {
+    console.log("clickedAitePlayer :" + x + "," + y);
+    console.log(typeof x);
+    console.log(typeof y);
+    const color = turn ? BLACK : WHITE;
+    console.log("color:" + color);
+    console.log("myCloer:" + myCloer);
+    if (color === myCloer) {
+        console.log("color違反");
+        return;
+    }
+    // マスに置けるかチェック
+    if (data[y][x] !== 0) {
+        console.log("マス違反");
+        return;
+    }
+    const result = checkPut(x, y, color);
+    console.log("checkPut2:" + result.length);
     if (result.length > 0) {
         result.forEach((value) => {
             putDisc(value[0], value[1], color);
@@ -152,7 +269,6 @@ function clicked() {
     showTurn();
 }
 
-// 置いたマスの周囲8方向をチェック
 function checkPut(x, y, color) {
     let dx, dy;
     const opponentColor = color == BLACK ? WHITE : BLACK;
@@ -215,12 +331,11 @@ function checkPut(x, y, color) {
     return reverseDisk;
 }
 
-// 裏返せる場所があるか確認
 function checkReverse(color) {
     for (let x = 0; x < cells; x++) {
         for (let y = 0; y < cells; y++) {
             const result = checkPut(x, y, color);
-            console.log(result);
+            // console.log(result);
             if (result.length > 0) {
                 return true;
             }
@@ -229,7 +344,6 @@ function checkReverse(color) {
     return false;
 }
 
-// ゲーム終了画面
 function restartBtn() {
     const restartBtn = document.getElementById("restartBtn");
     restartBtn.classList.remove("hide");
@@ -244,4 +358,9 @@ function restartBtn() {
 }
 function showAnime() {
     h2.animate({ opacity: [0, 1] }, { duration: 500, iterations: 4 });
+}
+
+function sleep(waitSec, callbackFunc) {
+    console.log("sleep");
+    setTimeout(callbackFunc, waitSec * 1000);
 }
